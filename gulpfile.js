8014e1ed26df0b7dc7e45f0a8f370a9e37d350b8
Gleb <git@gleb.nl>
2016-09-26 16:08:05 +0200

    Added gulp autoprefixer task to prefix keyanimations.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var embedTemplates = require('gulp-angular-embed-templates');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
  
 gulp.task('build', function () {
@@ -13,6 +14,10 @@ gulp.task('build', function () {
     .pipe(concat('text-animation.min.js'))
     .pipe(gulp.dest('./dist'));
   gulp.src(['src/text-animation.css'])
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
     .pipe(cssnano())
     .pipe(concat('text-animation.min.css'))
     .pipe(gulp.dest('./dist'));
