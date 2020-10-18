4fe870567f83ea84db52c58853e20c05613e8693
g1eb <mail@g1eb.com>
2020-10-18 12:17:37 -0700

    Update gulpfile with a breakdown of various build tasks and series

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,35 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
+var uglify = require('gulp-uglify-es').default;
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('build', function () {
-  gulp.src(['src/text-animation.js'])
+function buildJS() {
+  return gulp.src(['src/text-animation.js'])
     .pipe(uglify())
     .pipe(concat('text-animation.min.js'))
     .pipe(gulp.dest('./dist'));
-  gulp.src(['src/text-animation.css'])
+};
+
+function buildCSS() {
+  return gulp.src(['src/text-animation.css'])
     .pipe(autoprefixer({
         cascade: false
     }))
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(concat('text-animation.min.css'))
     .pipe(gulp.dest('./dist'));
-});
+};
 
-gulp.task('watch', function() {
-  gulp.watch('src/**/*.+(js|html|css)', ['build']);
-});
+function watch() {
+  return gulp.task('watch', function() {
+    gulp.watch('src/**/*.+(js|html|css)', ['build']);
+  });
+};
+
+exports.watch = watch;
+exports.buildJS = buildJS;
+exports.buildCSS = buildCSS;
+exports.build = gulp.series(buildJS, buildCSS);
+exports.default = gulp.series(watch);
